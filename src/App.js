4fb5f33425e1f190a07a04c2598457b9375c4eca
Mansur Mabo <mansurmabo@gmail.com>
2022-09-02 12:11:20 +0600

    add quiz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import { v4 as uuid } from 'uuid';
 import './App.css';
 
 function App() {
+  const [question, setQuestion] = useState('')
+  const [questionError, setQuestionError] = useState('') 
+  const [questionList, setQuestionList] = useState([]);
+  const [activeQuestion, setActiveQuestion] = useState(null)
+
+  const handleChangeQuestion = (event) => {
+    const { value } = event.target
+    setQuestion(value)
+  }
+
+  const handleAddQuestion = () => {
+    const trimmed = question.trim();
+    if (!trimmed) {
+      setQuestionError("Question can't be empty")
+      return;
+    }
+
+    const payload = {
+      name: question,
+      id: uuid()
+    }
+    const updatedQuestion = [...questionList, payload];
+    
+    setQuestionList(updatedQuestion)
+    setQuestion('')
+    setQuestionError('')
+  }
+
+  const handleRemoveQuestion = (id) => {
+    // const filtered = questionList.filter(q => q.id !== id)
+    const updatedQuestion = [...questionList];
+    const targetIndex = questionList.findIndex(q => q.id === id);
+    updatedQuestion.splice(targetIndex, 1)
+
+    setQuestionList(updatedQuestion);
+  }
+
+  const handleQuestionClick = (question) => {
+    setActiveQuestion(question)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Quiz</h1>
+      <div className='quiz-form'>
+        <input value={question} onChange={handleChangeQuestion} type="text"/>
+        <button onClick={handleAddQuestion}>Add</button>
+        {questionError && <div className={'error'}>{questionError}</div>}
+      </div>
+
+      <div className='questions'>
+        {questionList.map(q => (
+          <div key={q.id}>
+            <span className="question-name" onClick={() => handleQuestionClick(q)}>{q.name}</span> 
+            <button onClick={() => handleRemoveQuestion(q.id)}>x</button>
+          </div>
+        ))}
+      </div>
+
+      {activeQuestion && <div id="myModal" className="modal">
+        <div className="modal-content">
+          <span className="close" onClick={() => setActiveQuestion(null)}>&times;</span>
+          <p>{activeQuestion.id}</p>
+          <p>{activeQuestion.name}</p>
+        </div>
+      </div>
+      }
     </div>
   );
 }
